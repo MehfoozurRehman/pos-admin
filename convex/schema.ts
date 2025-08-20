28e441bc6d0dc10a4e4b4ddf594ae919676de4c9
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2025-08-20 18:40:10 +0500

    Create schema.ts

---
+++ convex/schema.ts
@@ -0,0 +1,50 @@
+import { defineSchema, defineTable } from "convex/server";
+
+import { v } from "convex/values";
+
+export default defineSchema({
+  shops: defineTable({
+    name: v.string(),
+    logo: v.string(),
+    location: v.string(),
+    owner: v.string(),
+    shopId: v.string(),
+    password: v.string(),
+    lastLogin: v.string(),
+  }),
+  payments: defineTable({
+    shop: v.id("shops"),
+    month: v.string(),
+    year: v.string(),
+  }),
+  products: defineTable({
+    shop: v.id("shops"),
+    name: v.string(),
+    description: v.string(),
+    category: v.optional(v.string()),
+  }),
+  variants: defineTable({
+    productId: v.string(),
+    product: v.id("products"),
+    name: v.string(),
+    actualPrice: v.number(),
+    sellingPrice: v.number(),
+    pictures: v.array(v.string()),
+  }),
+  inventory: defineTable({
+    variant: v.id("variants"),
+    quantity: v.number(),
+  }),
+  orders: defineTable({
+    shop: v.id("shops"),
+    status: v.string(),
+    customerName: v.string(),
+    customerPhone: v.string(),
+    items: v.array(
+      v.object({
+        variant: v.id("variants"),
+        keys: v.array(v.string()),
+      })
+    ),
+  }),
+});
