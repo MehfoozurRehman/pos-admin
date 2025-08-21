50fb1b106decf9c46ebb8acc1b3788967c69cad6
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2025-08-21 11:47:33 +0500

    Update layout.tsx

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,9 +1,28 @@
 import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar';
 
 import { AppSidebar } from '@/components/app-sidebar';
+import { COOKIE_TOKEN } from '@/config';
 import { SiteHeader } from '@/components/site-header';
+import { api } from '@/convex/_generated/api';
+import { cookies } from 'next/headers';
+import { fetchMutation } from 'convex/nextjs';
+import { redirect } from 'next/navigation';
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default async function Layout({ children }: { children: React.ReactNode }) {
+  const cookie = await cookies();
+
+  const token = cookie.get(COOKIE_TOKEN)?.value;
+
+  if (!token) {
+    return redirect('/?error=unauthorized');
+  }
+
+  const validUser = await fetchMutation(api.auth.validateUser, { id: token });
+
+  if (!validUser) {
+    return redirect('/?error=token_invalid');
+  }
+
   return (
     <SidebarProvider
       style={
