bb151c061bd42f5294bdf06fdcab2eaad2be3ce9
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2025-08-21 11:30:21 +0500

    Update auth.ts

--- convex/auth.ts
+++ convex/auth.ts
@@ -1,3 +1,5 @@
+import { hashPassword, verifyPassword } from '@/utils/encryption';
+
 import { mutation } from './_generated/server';
 import { v } from 'convex/values';
 
@@ -6,8 +8,43 @@ export const createAdmin = mutation({
   handler: async (ctx, args) => {
     const { name, email, password } = args;
 
-    const adminId = await ctx.db.insert('users', { name, email, password });
+    const hashedPassword = await hashPassword(password);
+
+    const adminId = await ctx.db.insert('users', { name, email, password: hashedPassword });
 
     return adminId;
+  },
+});
+
+export const loginAdmin = mutation({
+  args: { email: v.string(), password: v.string() },
+  handler: async (ctx, args) => {
+    const { email, password } = args;
+
+    if (!email) {
+      throw new Error('Email is required');
+    }
+
+    if (!password) {
+      throw new Error('Password is required');
+    }
+
+    const user = await ctx.db
+      .query('users')
+      .withIndex('by_email')
+      .filter((q) => q.eq(q.field('email'), email))
+      .first();
+
+    if (!user) {
+      throw new Error('User not found');
+    }
+
+    const validPassword = await verifyPassword(password, user.password);
+
+    if (!validPassword) {
+      throw new Error('Invalid password');
+    }
+
+    return user._id;
   },
 });
