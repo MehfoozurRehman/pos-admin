8b1c997d264f4ddb92114e08d9ff1cd4e5cb2abd
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2025-08-21 11:12:17 +0500

    Update route.ts

--- app/api/create-admin/route.ts
+++ app/api/create-admin/route.ts
@@ -1,15 +1,11 @@
 import { NextResponse } from 'next/server';
-
-// import { api } from '@/convex/_generated/api';
-// import bcrypt from 'bcrypt';
-// import { fetchMutation } from 'convex/nextjs';
+import { api } from '@/convex/_generated/api';
+import { fetchMutation } from 'convex/nextjs';
 
 export async function POST(request: Request) {
-  // const { name, email, password } = await request.json();
+  const { name, email, password } = await request.json();
 
-  // const hashedPassword = await bcrypt.hash(password, 12);
+  const adminId = await fetchMutation(api.auth.createAdmin, { name, email, password });
 
-  // const adminId = await fetchMutation(api.auth.createAdmin, { name, email, password: hashedPassword });
-
-  return NextResponse.json({});
+  return NextResponse.json({ adminId });
 }
